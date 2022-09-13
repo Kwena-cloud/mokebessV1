0f945295de3bc0007e78f4ff76ca4b23ea2a995e
Makitimelo Lekala <Makitimelo.lekala@boxfusion.io>
2022-09-13 21:47:55 +0200

    joel's changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,24 @@
 
-      $(document).ready(function(){
-        $('.menu-toggle').click(function(){
-          $('.menu-toggle').toggleClass('active')
-          $('.menu').toggleClass('active')
-        });
-      });
+$(document).ready(function () {
+  $('.menu-toggle').click(function () {
+    $('.menu-toggle').toggleClass('active')
+    $('.menu').toggleClass('active')
+  });
+});
+
+function readMore() {
+  var dots = document.getElementById("dots");
+  var moreText = document.getElementById("more");
+  var btnText = document.getElementById("readMoreBtn");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read more";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less";
+    moreText.style.display = "inline";
+  }
+}
 
